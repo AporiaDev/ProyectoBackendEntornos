a328b006018495984d036a7010249dca6f37b7b4
AporiaDev <169740552+AporiaDev@users.noreply.github.com>
2025-09-29 21:51:50 -0500

    CarpetUpdate

---
+++ entornosBackend/src/test/java/com/entornos/demo/ProyectobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entornos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
